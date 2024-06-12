7935ba6c5d22869ae0bee4d79cba298aa3b7dcc1
shahzahans <shahzahans@spu.edu>
2024-06-11 21:42:52 -0700

    adding more functions

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -4,6 +4,15 @@
 
 #include "tictactoe.h"
 
+tictactoe::tictactoe() {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
+            board[i][j] = ' ';
+        }
+    }
+    playermoves = 'X';
+}
+
 void tictactoe::myTable() {
     cout << "\tTic Toe Game\n";
     cout << "\t   X | O\n\n";
@@ -12,16 +21,9 @@ void tictactoe::myTable() {
     cout << "\t___|___|___" << endl;
     cout << "\t___|___|___" << endl;
     cout << "\t   |   |   " << endl;
-
 }
 
 bool tictactoe::gameConditions() {
-    if (board[0][0] == playermoves && board[1][1] == playermoves && board[2][2]){
-        return true;
-    }
-    if (board[0][2] == playermoves && board[1][1] == playermoves && board[2][0]){
-        return true;
-    }
     for (int i = 0; i < 0; i++){
         if (board[i][0] == playermoves && board[i][1] == playermoves && board[i][2]){
             return true;
@@ -29,6 +31,9 @@ bool tictactoe::gameConditions() {
         if (board[0][i] == playermoves && board[1][i] == playermoves && board[2][i]){
             return true;
         }
+    }
+    if ((board[0][0] == playermoves && board[1][1] == playermoves && board[2][2]) || (board[0][2] == playermoves && board[1][1] == playermoves && board[2][0])){
+        return true;
     }
     return false;
 }
