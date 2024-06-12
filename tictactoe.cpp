b518148de7097a41810b4dd294437b732bf38d2a
shahzahans <shahzahans@spu.edu>
2024-06-12 15:25:15 -0700

    Finished but need add comments

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,11 +2,12 @@
 // Created by Shadman on 5/6/2024.
 //
 
+#include <limits>
 #include "tictactoe.h"
 
 tictactoe::tictactoe() {
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             board[i][j] = ' ';
         }
     }
@@ -14,12 +15,15 @@ tictactoe::tictactoe() {
 }
 
 void tictactoe::printBoard() {
-    cout << "\tTic Toe Game\n";
+    cout << "\tWelcome to Shad's Tic Toe Game\n";
     cout << "\t   X | O\n\n";
     for (int i = 0; i < 3; ++i) {
+        cout << "\t";
         for (int j = 0; j < 3; j++) {
             cout << " " << board[i][j];
+            if (j < 2) cout << " |";
         }
+        cout << "\n";
         cout << "\t";
         if (i < 2) cout << "---|---|---\n";
     }
@@ -28,14 +32,14 @@ void tictactoe::printBoard() {
 
 bool tictactoe::gameConditions() {
     for (int i = 0; i < 3; i++){
-        if (board[i][0] == playermoves && board[i][1] == playermoves && board[i][2]){
+        if (board[i][0] == playermoves && board[i][1] == playermoves && board[i][2] == playermoves){
             return true;
         }
-        if (board[0][i] == playermoves && board[1][i] == playermoves && board[2][i]){
+        if (board[0][i] == playermoves && board[1][i] == playermoves && board[2][i] == playermoves){
             return true;
         }
     }
-    if ((board[0][0] == playermoves && board[1][1] == playermoves && board[2][2]) || (board[0][2] == playermoves && board[1][1] == playermoves && board[2][0])){
+    if ((board[0][0] == playermoves && board[1][1] == playermoves && board[2][2] == playermoves) || (board[0][2] == playermoves && board[1][1] == playermoves && board[2][0] == playermoves)){
         return true;
     }
     return false;
@@ -62,38 +66,45 @@ bool tictactoe::Draw(){
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             if (board[i][j] == ' '){
-                return true;
+                return false;
             }
         }
     }
-    return false;
+    return true;
 }
 
-void tictactoe::play(){
+void tictactoe::play() {
     int row, col;
     bool gameOver = false;
-    while (!gameOver){
+    while (!gameOver) {
         printBoard();
-        cout << "Player " << playermoves << ", enter your move (row & coloumn)";
-        cin >> row >> col;
+        cout << "Player " << playermoves << ", enter your move (row & column): ";
+        while (!(cin >> row >> col) || row < 0 || row > 2 || col < 0 || col > 2) {
+            cout << "Invalid input. Please enter row and column numbers (0-2): ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
 
-        if (marking(row, col)){
-            if (gameConditions()){
+        if (marking(row, col)) {
+            if (gameConditions()) {
                 printBoard();
                 cout << "Player " << playermoves << " wins!\n";
                 gameOver = true;
             }
-            else if (Draw()){
+            else if (Draw()) {
                 printBoard();
-                cout << "!GAME IS DRAWN!";
-                gameOver =true;
+                cout << "Game is drawn!\n";
+                gameOver = true;
             }
             else {
                 playerTurn();
             }
         }
         else {
-            cout << "Wrong Moves Made! Try Again\n";
+            cout << "Cell already occupied. Try again.\n";
         }
+
+        // Clear input buffer
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 }
